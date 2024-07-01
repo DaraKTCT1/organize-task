8f58af3e829f43e5b24046f181a7d05655146acb
daralabtop <150508323+daralabtop@users.noreply.github.com>
2024-07-01 21:53:47 +0700

    revalidate 2

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="w-full flex justify-center items-center mt-8">
-      <SignIn fallbackRedirectUrl="/create" />
+      <SignIn forceRedirectUrl={"/create"} />
     </div>
   );
 }
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="w-full flex justify-center items-center mt-8">
-      <SignUp fallbackRedirectUrl="/sign-in" />
+      <SignUp forceRedirectUrl={"/create"} />
     </div>
   );
 }
