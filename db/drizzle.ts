b902ae139e720960f8a403b31b1bb76760e6d614
daralabtop <150508323+daralabtop@users.noreply.github.com>
2024-07-02 11:11:30 +0700

    add type.ts

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -2,8 +2,6 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
-const sql = neon(
-  "postgresql://todos_owner:yEGdCk2Sqo6W@ep-floral-band-a55lmw5y-pooler.us-east-2.aws.neon.tech/todos?sslmode=require"
-);
+const sql = neon(process.env.NEON_DATABASE_URL!);
 
 export const db = drizzle(sql, { schema });
